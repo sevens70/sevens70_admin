e280c1f4d0019827ca7f198b733fe09b6b953b64
anamul01988 <anamulhaque0198877@gmail.com>
2024-10-31 20:01:29 +0600

    comment is removed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,13 +13,6 @@ const Header = (props: {
   sidebarOpen: string | boolean | undefined;
   setSidebarOpen: (arg0: boolean) => void;
 }) => {
-  // const router = useRouter();
-  // const user = useSelector(selectLoggedInUser);
-  // useEffect(() => {
-  //   if (!user) {
-  //     router.push("/auth/signin");
-  //   }
-  // }, [user, router]);
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
