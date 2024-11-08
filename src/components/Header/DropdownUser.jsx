fce5a7e2dc7a5400667f04a7870034c7f8ccbddc
anamul01988 <anamulhaque0198877@gmail.com>
2024-11-08 17:05:31 +0600

    admin dashboard enhancement done

--- src/components/Header/DropdownUser.jsx
+++ src/components/Header/DropdownUser.jsx
@@ -19,12 +19,12 @@ const DropdownUser = () => {
         className="flex items-center gap-4"
         href="#"
       >
-        <span className="hidden text-right lg:block">
+        {/* <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
             {user?.name ?? "Anamul Haq"}
           </span>
           <span className="block text-xs">Developer</span>
-        </span>
+        </span> */}
 
         <span className="h-12 w-12 rounded-full">
           <Image
@@ -61,7 +61,7 @@ const DropdownUser = () => {
         <div
           className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark`}
         >
-          <ul className="flex flex-col gap-5 border-b border-stroke px-6 py-7.5 dark:border-strokedark">
+          {/* <ul className="flex flex-col gap-5 border-b border-stroke px-6 py-7.5 dark:border-strokedark">
             <li>
               <Link
                 href="/profile"
@@ -87,7 +87,7 @@ const DropdownUser = () => {
                 My Profile
               </Link>
             </li>
-          </ul>
+          </ul> */}
           <button
             onClick={handleLogout}
             className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
